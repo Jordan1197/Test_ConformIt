f635ae15f94e770f5c3efb78773947dfc23c542f
dev-1831154 <1831154@cchic.etu.cchic.ca>
2021-05-20 21:33:17 -0400

    implémentation des controllers

--- TestProgrammationConformit/TestProgrammationConformit/Controllers/CommentaireController.cs
+++ TestProgrammationConformit/TestProgrammationConformit/Controllers/CommentaireController.cs
@@ -1,13 +1,137 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using TestProgrammationConformit.Infrastructures;
+using TestProgrammationConformit.Models;
+using Microsoft.EntityFrameworkCore;
+using TestProgrammationConformit.DataAccessLayer;
 namespace TestProgrammationConformit.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class CommentaireController : Controller
     {
-        
+        private readonly ConformitContext _context;
+        DataAccessProvider dataProvider;
+        public CommentaireController(ConformitContext context)
+        {
+            _context = context;
+            dataProvider = new DataAccessProvider(context);
+        }
+
+        /// <summary>
+        /// recupere la liste complete des commentaires
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<IEnumerable<Commentaire>> Get()
+        {
+            List<Commentaire> commentaires= dataProvider.GetAllComms();
+            return Ok(commentaires);
+        }
+
+        /// <summary>
+        /// recupere un commentaire selon sont identifiant
+        /// </summary>
+        /// <param name="id">l'identifiant du commentaire</param>
+        /// <returns></returns>
+        [HttpGet("[action]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetById(int id)
+        {
+            Commentaire c;
+            c = dataProvider.GetCommsById(id);
+            if (c is null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(c);
+            }
+
+
+        }
+
+        /// <summary>
+        /// ajout d'un commentaire
+        /// </summary>
+        /// <param name="idEvent">id de l'evenement qui possede le commentaire</param>
+        /// <param name="commentaire">le nouveau commentaire</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Post(int idEvent,[FromBody] Commentaire commentaire)
+        {
+            Evenement evenement = dataProvider.GetEventById(idEvent);
+
+            if (commentaire != null)
+            {
+                commentaire.idevenement = idEvent;
+                dataProvider.AddComms(commentaire);
+
+                //evenement.listecommentaires.Add(commentaire);
+                return Ok(commentaire.description + " à bien été crée");
+            }
+            else
+            {
+                return UnprocessableEntity();
+            }
+
+        }
+
+        /// <summary>
+        /// permet la modification d'un commentaire selon son identifiant
+        /// </summary>
+        /// <param name="id">identifiant du commentaire</param>
+        /// <param name="commentaire">les donnees du nouveau commentaire</param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Put(int id, [FromBody] Commentaire commentaire)
+        {
+            if (commentaire != null)
+            {
+                commentaire.id = id;
+                dataProvider.UpdateComms(commentaire);
+                return Ok();
+            }
+            else
+            {
+                return UnprocessableEntity();
+            }
+
+        }
+
+        /// <summary>
+        /// supprime un commentaire selon son identifiant
+        /// </summary>
+        /// <param name="id">l'identifiant du commentaire</param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int id)
+        {
+            Commentaire c = dataProvider.GetCommsById(id);
+            if (c != null)
+            {
+                dataProvider.DeleteComms(id);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
     }
 }
--- TestProgrammationConformit/TestProgrammationConformit/Controllers/EvenementController.cs
+++ TestProgrammationConformit/TestProgrammationConformit/Controllers/EvenementController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TestProgrammationConformit.Infrastructures;
 using TestProgrammationConformit.Models;
+using Microsoft.EntityFrameworkCore;
 using TestProgrammationConformit.DataAccessLayer;
 
 namespace TestProgrammationConformit.Controllers
@@ -15,48 +16,119 @@ namespace TestProgrammationConformit.Controllers
     [Route("[controller]")]
     public class EvenementController : Controller
     {
-
-        private readonly IDataAccessProvider _dataAccessProvider;
-
-        public EvenementController(IDataAccessProvider dataAccessProvider)
+        private readonly  ConformitContext _context;
+        DataAccessProvider dataProvider;
+        public EvenementController(ConformitContext context)
         {
-            _dataAccessProvider = dataAccessProvider;
+            _context = context;
+            dataProvider = new DataAccessProvider(context);
         }
 
-        private readonly ILogger<EvenementController> _logger;
-        public EvenementController(ILogger<EvenementController> logger)
-        {
-            _logger = logger;
-        }
-
-
-
+        /// <summary>
+        /// recupere la liste complete des evenements
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<IEnumerable<Evenement>> Get()
         {
-            return _dataAccessProvider.GetAllEvenement();
+            List<Evenement> evenements = dataProvider.GetAllEvent();
+            return Ok(evenements);
         }
 
-        /*[HttpGet("[action]")]
+        /// <summary>
+        /// recupere un evenement selon sont identifiant
+        /// </summary>
+        /// <param name="id">l'identifiant de  l'evenement</param>
+        /// <returns></returns>
+        [HttpGet("[action]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetById(int id)
         {
-            var evenement = _context.Find<Evenement>(Convert.ToInt32(id));
-
-            return Ok(evenement);
-        }*/
-
-        [HttpPost]
-        public IActionResult Create([FromBody] Evenement evenement)
-        {
-            if (ModelState.IsValid)
+            Evenement e;
+            e= dataProvider.GetEventById(id);
+            if(e is null)
             {
-                _dataAccessProvider.AddEvenement(evenement);
+                return NotFound();
+            }
+            else
+            {
+                return Ok(e);
+            }
+
+           
+        }
+
+        /// <summary>
+        /// ajout d'un evenement
+        /// </summary>
+        /// <param name="evenement">le nouveau evenement</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Post([FromBody] Evenement evenement)
+        {
+            if(evenement != null)
+            {
+                dataProvider.AddEvent(evenement);
+
+                return Ok(evenement.titre + " à bien été crée");
+            }
+            else
+            {
+                return UnprocessableEntity();
+            }
+            
+        }
+
+        /// <summary>
+        /// permet la modification d'un evenement selon son identifiant
+        /// </summary>
+        /// <param name="id">identifiant de l'evenement</param>
+        /// <param name="evenement">les donnees du nouvelle evenement</param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Put(int id,[FromBody] Evenement evenement)
+        {
+            if(evenement != null)
+            {
+                evenement.id = id;
+                dataProvider.UpdateEvent(evenement);
                 return Ok();
             }
-            return BadRequest();
+            else
+            {
+                return UnprocessableEntity();
+            }
+            
+        }
 
+        /// <summary>
+        /// supprime un evenement selon son identifiant
+        /// </summary>
+        /// <param name="id">l'identifiant de l'evenement</param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int id)
+        {
+            Evenement e = dataProvider.GetEventById(id);
+            if (e != null)
+            {
+                dataProvider.DeleteEvent(id);
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+            
         }
     }
 }
