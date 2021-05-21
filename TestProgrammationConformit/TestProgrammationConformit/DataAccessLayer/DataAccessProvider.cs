d9f0ef0010ead875a2558cf13c53e3f7cfd06888
dev-1831154 <1831154@cchic.etu.cchic.ca>
2021-05-20 21:33:44 -0400

    implémentation DataAccessProvider

--- TestProgrammationConformit/TestProgrammationConformit/DataAccessLayer/DataAccessProvider.cs
+++ TestProgrammationConformit/TestProgrammationConformit/DataAccessLayer/DataAccessProvider.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using TestProgrammationConformit.Models;
 using TestProgrammationConformit.Infrastructures;
+using TestProgrammationConformit.Models;
 
 namespace TestProgrammationConformit.DataAccessLayer
 {
-    public class DataAccessProvider : IDataAccessProvider
+    public class DataAccessProvider
     {
         private readonly ConformitContext _context;
 
@@ -15,16 +15,69 @@ namespace TestProgrammationConformit.DataAccessLayer
         {
             _context = context;
         }
-
-        public void AddEvenement(Evenement evenement)
+        #region Evenements
+        public List<Evenement> GetAllEvent()
         {
-            _context.Evenements.Add(evenement);
+            return _context.evenement.ToList();
+        }
+
+        public Evenement GetEventById(int id)
+        {
+            var e = _context.evenement.Find(id);
+            Evenement evenement = e;
+            return e;
+        }
+
+        public void AddEvent(Evenement evenement)
+        {
+            _context.evenement.Add(evenement);
             _context.SaveChanges();
         }
 
-        public List<Evenement> GetAllEvenement()
+        public void UpdateEvent(Evenement evenement)
         {
-            return _context.Evenements.ToList();
+            //Evenement existingEvent = GetEventById(evenement.id);
+            _context.evenement.Update(evenement);
+            _context.SaveChanges();
+            
         }
+
+        public void DeleteEvent(int id)
+        {
+            _context.evenement.Remove(GetEventById(id));
+            _context.SaveChanges();
+        }
+        #endregion
+        #region Commentaires
+        public List<Commentaire> GetAllComms()
+        {
+            return _context.commentaire.ToList();
+        }
+
+        public Commentaire GetCommsById(int id)
+        {
+            var c = _context.commentaire.Find(id);
+            Commentaire comms = c;
+            return comms;
+        }
+
+        public void AddComms(Commentaire commentaire)
+        {
+            _context.commentaire.Add(commentaire);
+            _context.SaveChanges();
+        }
+
+        public void UpdateComms(Commentaire commentaire)
+        {
+            _context.commentaire.Update(commentaire);
+            _context.SaveChanges();
+        }
+
+        public void DeleteComms(int id)
+        {
+            _context.commentaire.Remove(GetCommsById(id));
+            _context.SaveChanges();
+        }
+        #endregion
     }
 }
